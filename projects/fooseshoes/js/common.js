5f9035b35d4b2504ca3603d67d24198c3e0ecaaf
kravczek-p <kravczek@gmail.com>
2019-02-24 18:34:40 +0200

    add portfolio

--- projects/fooseshoes/js/common.js
+++ projects/fooseshoes/js/common.js
@@ -1,10 +1,12 @@
+
+
 (function ($) {
     $(document).ready(function () {
         console.log('click');
         $('.mobile-menu').on('click', function () {
          
-            // $('.mobile-menu__icon').toggleClass('open');
-            $('.header__nav-menu').toggleClass('opened');
+            $('.mobile-menu__icon').toggleClass('open');
+            $('.menu__nav').toggleClass('opened');
         });
     });
 
--- projects/mealex/js/common.js
+++ projects/mealex/js/common.js
@@ -1,9 +1,9 @@
-(function ($) {
+;(function ($) {
     $(document).ready(function () {
         console.log('click');
         $('.mobile-menu').on('click', function () {
          
-            // $('.mobile-menu__icon').toggleClass('open');
+            $('.mobile-menu__icon').toggleClass('open');
             $('.header__nav-menu').toggleClass('opened');
         });
     });
